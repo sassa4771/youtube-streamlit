4edaccca78a52f003c1434ef6604b33344c04c31
sasatake <53072999+sassa4771@users.noreply.github.com>
2023-03-11 17:20:28 +0900

    second commit

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from PIL import Image
 st.title('Streamlit 入門')
 st.write('Display Image')
 
-img = Image.open('Demo.png')
+img = Image.open('demo.png')
 
 st.image(img, caption='demo', use_column_width=True)
