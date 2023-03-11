b69f871797cda9e61dec985ce12f774185b57fc9
sasatake <53072999+sassa4771@users.noreply.github.com>
2023-03-11 17:10:28 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+st.title('Streamlit 入門')
+st.write('Display Image')
+
+img = Image.open('Demo.png')
+
+st.image(img, caption='demo', use_column_width=True)
